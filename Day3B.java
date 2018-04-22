d823a8b34a781bd9d4fe4093053b9aa834571ff2
andressantamaria <andressantamariac@gmail.com>
2018-04-22 01:34:47 -0300

    Day3 modified

--- Day3B.java
+++ Day3B.java
@@ -7,6 +7,7 @@ If  n is even and in the inclusive range of 2 to 5, print Not Weird
 If  n is even and in the inclusive range of 6 to 20, print Weird
 If  n is even and greater than 20, print Not Weird
 Complete the stub code provided in your editor to print whether or not  is weird.
+was modifyed
  */
 package hacker.rank;
 
